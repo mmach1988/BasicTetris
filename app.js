2d7e016ca5c2a359572f3a2f7ed7439053d963ea
Maciej Machniewski <machniewski.maciej@gmail.com>
2020-10-22 18:45:58 +0200

    Zmiany 22.10

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log('DOM fully loaded and parsed');
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+    console.log(squares)
+});
