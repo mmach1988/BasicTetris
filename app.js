d556ad2bed2064dd141e7d9022d2d04e33064cf1
TomekoLeko <ays@vp.pl>
2020-10-29 17:43:30 +0100

    ltetromino added

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log('DOM fully loaded and parsed');
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+
     const width = 10
-    console.log(squares)
+    // console.log(squares)
+
+const lTetromino = [
+]
+
+
+
 });
