c9668e1cc5cbfd36a2e5a6182771753186d3f0f7
Maciej Machniewski <machniewski.maciej@gmail.com>
2020-10-29 18:49:11 +0100

    Uzupelnione eltetromina

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log('DOM fully loaded and parsed');
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
     const width = 10
     // console.log(squares)
 
 const lTetromino = [
+    [1,width+1,width*2+1,2],
+    [width,width+1,width+2,width*2+2],
+    [1,width+1,width*2+1,width*2],
+    [width,width*2,width*2+1,width*2+2]
 ]
 
 
