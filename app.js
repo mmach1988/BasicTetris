2a3878790011a8732f7c491fce827c57915160a1
TomekoLeko <ays@vp.pl>
2020-12-29 15:26:11 +0100

    nowe tetromina

--- app.js
+++ app.js
@@ -9,39 +9,40 @@ document.addEventListener('DOMContentLoaded', () => {
     let nextRandom = 0
     // console.log(squares)
 
-const lTetromino = [
+   //tetriminoes
+   const lTetromino = [
     [1,width+1,width*2+1,2],
     [width,width+1,width+2,width*2+2],
     [1,width+1,width*2+1,width*2],
     [width,width*2,width*2+1,width*2+2]
-]
+  ]
 
 const zTetromino = [
-    [width*2,width+1,width*2+1,width+2],
-    [0,width,width+1,width*2+1],
-    [width*2,width+1,width*2+1,width+2],
-    [0,width,width+1,width*2+1]
+  [0,width,width+1, width*2+1],
+  [width+1,width+2,width*2,width*2+1],
+  [0,width,width+1,width*2+1],
+  [width+1,width+2,width*2,width*2+1]
 ]
 
 const tTetromino = [
-    [1,width,width+1,width+2],
-    [1,width+1,width+2,width*2+1],
-    [width,width+1,width+2,width*2+1],
-    [1,width,width+1,width*2+1]
+  [1,width,width+1,width+2],
+  [1,width+1,width+2,width*2+1],
+  [width,width+1,width+2,width*2+1],
+  [1,width,width+1,width*2+1]
 ]
 
 const oTetromino = [
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1]
+  [0,1,width,width+1],
+  [0,1,width,width+1],
+  [0,1,width,width+1],
+  [0,1,width,width+1]
 ]
 
 const iTetromino = [
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3],
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3]
+  [1,width+1,width*2+1,width*3+1],
+  [width,width+1,width+2,width+3],
+  [1,width+1,width*2+1,width*3+1],
+  [width,width+1,width+2,width+3]
 ]
 
 const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
